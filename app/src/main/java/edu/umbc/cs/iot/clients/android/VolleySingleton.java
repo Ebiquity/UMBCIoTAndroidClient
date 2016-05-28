9b2c8eb666dee56078acb312370573508e500ec9
Prajit Kumar Das <prajit1@umbc.edu>
2016-05-28 04:50:05 -0400

    init commit

---
+++ app/src/main/java/edu/umbc/cs/iot/clients/android/VolleySingleton.java
@@ -0,0 +1,58 @@
+package edu.umbc.cs.iot.clients.android;
+
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by praji on 5/28/2016.
+ */
+
+public class VolleySingleton {
+    private static VolleySingleton mInstance;
+    private RequestQueue mRequestQueue;
+    private static Context mCtx;
+
+    private VolleySingleton(Context context) {
+        mCtx = context;
+        mRequestQueue = getRequestQueue();
+
+//        mImageLoader = new ImageLoader(mRequestQueue,
+//                new ImageLoader.ImageCache() {
+//                    private final LruCache<String, Bitmap>
+//                            cache = new LruCache<String, Bitmap>(20);
+//
+//                    @Override
+//                    public Bitmap getBitmap(String url) {
+//                        return cache.get(url);
+//                    }
+//
+//                    @Override
+//                    public void putBitmap(String url, Bitmap bitmap) {
+//                        cache.put(url, bitmap);
+//                    }
+//                });
+    }
+
+    public static synchronized VolleySingleton getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new VolleySingleton(context);
+        }
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            // getApplicationContext() is key, it keeps you from leaking the
+            // Activity or BroadcastReceiver if someone passes one in.
+            mRequestQueue = Volley.newRequestQueue(mCtx.getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+}
