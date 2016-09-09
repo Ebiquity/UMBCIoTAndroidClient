b5a622f1798e3b3bd404871310a63e7d01afae50
Prajit Kumar Das <prajitdas@gmail.com>
2016-09-09 03:15:25 -0400

    user id added

--- app/src/main/java/edu/umbc/cs/iot/clients/android/util/JSONRequest.java
+++ app/src/main/java/edu/umbc/cs/iot/clients/android/util/JSONRequest.java
@@ -14,26 +14,12 @@ public final class JSONRequest {
     private JSONObject request;
 
     public JSONRequest(String aQuery, String aBeasonId, String aSessionId, String aUserId) throws JSONException {
-        // Add your data
-        //Create JSONObject here
         request = new JSONObject();
         request.put(UMBCIoTApplication.getJsonQuestionKey(), aQuery);
         request.put(UMBCIoTApplication.getJsonBeaconKey(), aBeasonId);
         request.put(UMBCIoTApplication.getJsonSessionIdKey(), aSessionId);
         request.put(UMBCIoTApplication.getJsonUserIdKey(), aUserId);
-//        Toast.makeText(view.getContext(),"I have: "+mBeconID,Toast.LENGTH_LONG).show();
-
-//        JSONArray jsonArray = new JSONArray();
-//        for(String applicationInfo : getCurrentlyInstalledAppsList()) {
-//            jsonArray.put(applicationInfo);
-//        	  jsonArray.put("Facebook");
-//			  jsonArray.put("Twitter");
-//        }
-//        jsonParam.put("currentApps",jsonArray);
-
-//        return request.toString();
     }
-
     public JSONObject getRequest() {
         return request;
     }
