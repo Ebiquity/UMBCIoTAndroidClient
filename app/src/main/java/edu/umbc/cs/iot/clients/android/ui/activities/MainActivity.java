7ba35564c2ded523a71b4ad4a907dac3986cafef
Prajit Kumar Das <prajitdas@gmail.com>
2017-04-02 22:46:50 -0400

    added feedback buttons... testing... testing...
    Signed-off-by: Prajit Kumar Das <prajitdas@gmail.com>

--- app/src/main/java/edu/umbc/cs/iot/clients/android/ui/activities/MainActivity.java
+++ app/src/main/java/edu/umbc/cs/iot/clients/android/ui/activities/MainActivity.java
@@ -23,7 +23,6 @@ import android.os.Handler;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
@@ -105,8 +104,6 @@ public class MainActivity extends AppCompatActivity implements
     private String beaconData;
     private MessageListener mMessageListener;
     private View headerView;
-    private FloatingActionButton fabThumbUp;
-    private FloatingActionButton fabThumbDown;
     private FrameLayout mContainer;
 
     @Override
@@ -129,9 +126,6 @@ public class MainActivity extends AppCompatActivity implements
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, UMBCIoTApplication.PERMISSIONS_REQUEST_BLUETOOTH);
         }
-
-        fabThumbUp = (FloatingActionButton) findViewById(R.id.fab_up);
-        fabThumbDown = (FloatingActionButton) findViewById(R.id.fab_down);
 
         mContainer = (FrameLayout) findViewById(R.id.container_main);
 
@@ -402,17 +396,11 @@ public class MainActivity extends AppCompatActivity implements
                  * and add the transaction to the back stack if needed
                  */
                 transaction.replace(R.id.container_main, fragment);
-                fabThumbUp.setVisibility(View.VISIBLE);
-                fabThumbDown.setVisibility(View.VISIBLE);
             } else {
                 transaction.replace(R.id.container_main, new EmptyFragment());
-                fabThumbUp.setVisibility(View.GONE);
-                fabThumbDown.setVisibility(View.GONE);
             }
         } else {
             transaction.replace(R.id.container_main, fragment);
-            fabThumbUp.setVisibility(View.GONE);
-            fabThumbDown.setVisibility(View.GONE);
         }
         transaction.addToBackStack(null);
         // Commit the transaction
